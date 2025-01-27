91bd7bd89060dbdf5ecdccfc4655833d2804e24e
BrenonDev <brenon.olivetti.rondello@gmail.com>
2025-01-27 18:19:32 -0300

    atualização de currículo

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Função para alternar o modo
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
@@ -7,21 +6,20 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
-    localStorage.setItem("theme", "light") // Salva o modo light no localStorage
+    localStorage.setItem("theme", "light")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
-    localStorage.setItem("theme", "dark") // Salva o modo dark no localStorage
+    localStorage.setItem("theme", "dark")
   }
 }
 
-// Função para carregar o tema salvo
 function loadTheme() {
-  const savedTheme = localStorage.getItem("theme") // Verifica se há um tema salvo no localStorage
+  const savedTheme = localStorage.getItem("theme")
   const html = document.documentElement
   const img = document.querySelector("#profile img")
 
   if (savedTheme) {
-    html.classList.toggle("light", savedTheme === "light") // Define o tema de acordo com o valor salvo
+    html.classList.toggle("light", savedTheme === "light")
     if (savedTheme === "light") {
       img.setAttribute("src", "./assets/avatar-light.png")
     } else {
@@ -30,5 +28,4 @@ function loadTheme() {
   }
 }
 
-// Chama a função ao carregar a página para definir o tema corretamente
 loadTheme()
